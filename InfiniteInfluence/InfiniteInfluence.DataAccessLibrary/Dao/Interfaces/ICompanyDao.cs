2c8ddd7bd801394e29ee02cc197d5a0d4cb4acce
Line-B-333 <10187764@ucn.dk>
2025-11-15 19:25:48 +0100

    Created: ICompanyDao with CreateCompany, GetoOneCompany and DeleteCompany

---
+++ InfiniteInfluence/InfiniteInfluence.DataAccessLibrary/Dao/Interfaces/ICompanyDao.cs
@@ -0,0 +1,18 @@
+﻿using InfiniteInfluence.DataAccessLibrary.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfiniteInfluence.DataAccessLibrary.Dao.Interfaces;
+
+public interface ICompanyDao
+{
+    int CreateCompany(Company company);
+
+    Company? GetOneCompany(int userId);
+
+    bool DeleteCompany(int userId);
+
+}
