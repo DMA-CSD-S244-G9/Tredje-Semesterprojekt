c64a4710ac68d9532aa57888e68cdf7cdc0c6a88
Line-B-333 <10187764@ucn.dk>
2025-11-15 21:23:38 +0100

    Added: Program

--- InfiniteInfluence/InfiniteInfluence.API/Program.cs
+++ InfiniteInfluence/InfiniteInfluence.API/Program.cs
@@ -7,7 +7,7 @@ namespace InfiniteInfluence.API;
 public class Program
 {
     // The database connection string used for dependency injection of any of the DAO classes (InfluencerDao, CompanyDao etc.)
-    private const string _dataBaseConnectionString = "Data Source=localhost;Initial Catalog=BlogSharp2025;Persist Security Info=True;User ID=sa;Password=@12tf56so;Encrypt=True;Trust Server Certificate=True";
+    private const string _dataBaseConnectionString = "Data Source=localhost;Initial Catalog=InfiniteInfluence;Persist Security Info=True;User ID=sa;Password=@12tf56so;Encrypt=True;Trust Server Certificate=True";
 
 
 
@@ -27,6 +27,8 @@ public class Program
         // Register the AuthorDao for MSSqlServer for dependency injection
         builder.Services.AddScoped<IInfluencerDao>((_) => new InfluencerDao(_dataBaseConnectionString));
 
+        // Register the CompanyDao for MSSqlServer for dependency injection
+        builder.Services.AddScoped<ICompanyDao>((_) => new CompanyDao(_dataBaseConnectionString));
 
 
 
