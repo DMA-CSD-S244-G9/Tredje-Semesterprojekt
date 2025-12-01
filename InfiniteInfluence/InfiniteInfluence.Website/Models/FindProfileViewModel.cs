d5ee513ad28f95b4cab8569e21b96bb265452da8
Line-B-333 <10187764@ucn.dk>
2025-12-01 19:12:23 +0100

    Created: Viewmodel

---
+++ InfiniteInfluence/InfiniteInfluence.Website/Models/FindProfileViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InfiniteInfluence.Website.Models
+{
+    public class FindProfileViewModel
+    {
+        // This is temporary until we have some sort of user validation or login system
+        [Required]
+        [Display(Name = "User Id")]
+        [RegularExpression("^[0-9]+$", ErrorMessage = "You must enter numbers only.")]
+        [Range(1, int.MaxValue, ErrorMessage = "A valid User Id is required.")]
+        public int UserId { get; set; }
+    }
+}
