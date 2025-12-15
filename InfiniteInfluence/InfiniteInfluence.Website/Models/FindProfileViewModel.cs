f0cc801f3420de111e2c38b6a1ab6e66189f8de0
Line-B-333 <10187764@ucn.dk>
2025-12-15 18:44:29 +0100

    Updated: FindProfileViewModel removed comment

--- InfiniteInfluence/InfiniteInfluence.Website/Models/FindProfileViewModel.cs
+++ InfiniteInfluence/InfiniteInfluence.Website/Models/FindProfileViewModel.cs
@@ -10,7 +10,6 @@ namespace InfiniteInfluence.Website.Models;
 /// </summary>
 public class FindProfileViewModel
 {
-    // This is temporary until we have some sort of user validation or login system
     [Required(ErrorMessage = "User Id is required.")]
     [Display(Name = "User Id")]
     [Range(1, int.MaxValue, ErrorMessage = "A valid User Id is required.")]
