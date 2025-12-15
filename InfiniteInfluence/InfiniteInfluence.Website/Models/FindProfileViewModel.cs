5ea4b9e39b572e9116d9035e5b5831d7045d2201
Line-B-333 <10187764@ucn.dk>
2025-12-15 17:39:35 +0100

    Added: Findprofile - type number, numeric and comment

--- InfiniteInfluence/InfiniteInfluence.Website/Models/FindProfileViewModel.cs
+++ InfiniteInfluence/InfiniteInfluence.Website/Models/FindProfileViewModel.cs
@@ -9,7 +9,6 @@ public class FindProfileViewModel
     // This is temporary until we have some sort of user validation or login system
     [Required(ErrorMessage = "User Id is required.")]
     [Display(Name = "User Id")]
-    //[RegularExpression("^[0-9]+$")]
     [Range(1, int.MaxValue, ErrorMessage = "A valid User Id is required.")]
     public int UserId { get; set; }
 }
